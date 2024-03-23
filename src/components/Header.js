863c760c3ae0d2c41a3e588f11627af788da0066
Maham <kmrajput3412@gmail.com>
2024-03-23 20:17:02 +0500

    Changings

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+      <h1>React Art</h1>
       <p>Designing React App</p>
     </header>
   );
